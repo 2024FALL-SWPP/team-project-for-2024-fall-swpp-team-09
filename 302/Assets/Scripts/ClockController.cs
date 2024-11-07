e493b0293c60fc2c7a967a87cef954fc06b46c9c
seozzi <seojihee1004@snu.ac.kr>
2024-11-07 19:23:41 +0900

    ClockController.cs 개발 및 Clock object에 할당

--- 302/Assets/Scripts/ClockController.cs
+++ 302/Assets/Scripts/ClockController.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ClockController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Transform hourHand;
+    private Transform minuteHand;
+
     void Start()
     {
-        
+        hourHand = transform.Find("H");
+        minuteHand = transform.Find("M");
+
+        // initial rotation to 12:00
+        hourHand.localRotation = Quaternion.Euler(0, 0, 90); 
+        minuteHand.localRotation = Quaternion.Euler(0, 0, 90);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetTime(int hour, int minute)
     {
-        
+        // Ensure hour is in 12-hour format
+        hour %= 12;
+
+        float minuteRotation = minute * 6f; // 6 degrees per minute
+        float hourRotation = (hour * 30f) + (minute * 0.5f); // 30 degrees per hour + 0.5 degrees per minute
+
+        minuteHand.localRotation = Quaternion.Euler(0, 0, 90 + minuteRotation);
+        hourHand.localRotation = Quaternion.Euler(0, 0, 90 + hourRotation);
     }
 }
