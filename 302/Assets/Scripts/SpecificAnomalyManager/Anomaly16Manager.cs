c356ca7d68f9880196453abc20681c4984cd2a26
seozzi <seojihee1004@snu.ac.kr>
2024-11-12 17:13:49 +0900

    이상현상 16(벽에 그려지는 마커 이상현상) 구현

--- 302/Assets/Scripts/SpecificAnomalyManager/Anomaly16Manager.cs
+++ 302/Assets/Scripts/SpecificAnomalyManager/Anomaly16Manager.cs
@@ -1,18 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Anomaly16Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Marker Line Settings")]
+    public Material redLineMaterial;  // Assign a red material in the Inspector
+    public AudioClip markerSound;     // Assign the marker sound in the Inspector
+    private GameObject markerLine;
+    private AudioSource audioSource;
+
+    private void Start()
     {
-        
+        // Get the AudioSource component assigned in the Inspector
+        audioSource = GetComponent<AudioSource>();
+
+        // Check if audioSource is assigned
+        if (audioSource == null)
+        {
+            Debug.LogError("AudioSource component is missing from Anomaly16Manager!");
+            return;
+        }
+
+        CreateMarkerLineInstance();
+        AddMarkerScript();
+        SetLayerForInteraction();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateMarkerLineInstance()
     {
-        
+        // Create the MarkerLine GameObject
+        markerLine = new GameObject("MarkerLine");
+
+        // Add and configure LineRenderer component
+        LineRenderer lineRenderer = markerLine.AddComponent<LineRenderer>();
+        lineRenderer.material = redLineMaterial;
+        lineRenderer.startColor = Color.red;
+        lineRenderer.endColor = Color.red;
+        lineRenderer.startWidth = 0.05f;
+        lineRenderer.endWidth = 0.05f;
+
+        // Add a MeshCollider
+        MeshCollider meshCollider = markerLine.AddComponent<MeshCollider>();
+        meshCollider.convex = false;
+    }
+
+    private void AddMarkerScript()
+    {
+        // Add Anomaly16_marker component for interaction
+        Anomaly16_marker markerScript = markerLine.AddComponent<Anomaly16_marker>();
+        markerScript.markerSoundClip = markerSound;
+    }
+
+    private void SetLayerForInteraction()
+    {
+        // Set the layer for interactions
+        markerLine.layer = 3;
+    }
+
+    // Function to destroy markerLine with disappearing sound
+    public void DestroyMarkerLineWithSound()
+    {
+        // Play the disappearing sound
+        audioSource.Play();
+
+        // Destroy markerLine after the sound finishes playing
+        Destroy(markerLine);
     }
 }
